f3e986b0152803c58cfeff747c2f79f6f4b30ddc
kit rio <mario64aq@gmail.com>
2019-10-18 23:32:06 +0900

    Add TV dmb Tile TilService
    android QuickTile button

---
+++ app/src/main/java/com/jw/studio/quicktile/TVTile.java
@@ -0,0 +1,40 @@
+package com.jw.studio.quicktile;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+
+public class TVTile extends TileService {
+    @Override
+    public void onClick() {
+        super.onClick();
+        ComponentName componentName = new ComponentName("com.lge.tdmb","com.lge.tdmb.TdmbMainScreenActivity");
+        try {
+            startActivityAndCollapse(getActivityIntent(componentName));
+        }catch (Exception e){
+            getApplicationContext().sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+            Toast.makeText(this, getString(R.string.is_support), Toast.LENGTH_SHORT).show();
+        }
+        onStopListening();
+    }
+
+    @Override
+    public void onTileRemoved(){
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onTileAdded(){
+        super.onTileAdded();
+    }
+
+    public static Intent getActivityIntent(ComponentName activity) {
+        Intent intent = new Intent();
+        intent.setComponent(activity);
+        intent.putExtra("com.lge.app.floating.launchAsFloating", true);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
+}
