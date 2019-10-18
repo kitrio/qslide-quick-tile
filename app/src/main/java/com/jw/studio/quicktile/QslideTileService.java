02fd6e693019540d2fe159709c8a820199e6127e
kit rio <mario64aq@gmail.com>
2019-10-18 16:24:47 +0900

    Remove useless comment

--- app/src/main/java/com/jw/studio/quicktile/QslideTileService.java
+++ app/src/main/java/com/jw/studio/quicktile/QslideTileService.java
@@ -4,16 +4,14 @@ package com.jw.studio.quicktile;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.service.quicksettings.TileService;
-import android.util.Log;
 import android.widget.Toast;
 
 public class QslideTileService extends TileService {
+
     @Override
     public void onClick() {
         super.onClick();
-        Log.d("aoa","aaa");
         ComponentName componentName = new ComponentName("com.lge.filemanager","com.lge.filemanager.view.FloatingActivity");
-        //ComponentName componentName = new ComponentName("com.jw.qslideview","com.jw.qslideview.MainActivity");
         try {
             startActivityAndCollapse(getActivityIntent(componentName));
         }catch (Exception e){
@@ -32,15 +30,7 @@ public class QslideTileService extends TileService {
     public void onTileAdded(){
         super.onTileAdded();
     }
-//    @Override
-//    public void onStartListening() {
-//        super.onStartListening();
-////        Tile tile = getQsTile();
-////        tile.setLabel(getString(R.string.filemgr));
-////        tile.setState(Tile.STATE_ACTIVE);
-////        tile.setIcon(Icon.createWithResource(this,R.drawable.ic_folder_open_black_24dp));
-////        tile.updateTile();
-//    }
+
     public static Intent getActivityIntent(ComponentName activity) {
         Intent intent = new Intent();
         intent.setComponent(activity);
